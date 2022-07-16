3a3551cabfbd2183ed4ea004668fb3b451776c18
syedkhalidashraf <i.khalidashraf@gmail.com>
2022-07-15 22:40:50 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import './style.css';
-import API from './api';
+import React from "react";
+import "./style.css";
+import API from "./api";
 
 const App = () => {
-  return (
-    <div className="App">
-      <form id="form">
-        <label>
-          Rider UUID:
-          <input type="text" id="text" value="TEST" />
-        </label>
-        <input type="submit" value="Fetch driver emails" />
-        <div id="results" />
-      </form>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<form
+				id='form'
+				onSubmit={() => {
+					console.log("hello");
+				}}>
+				<label>
+					Rider UUID:
+					<input type='text' id='text' value='TEST' />
+				</label>
+				<input type='submit' value='Fetch driver emails' />
+				<div id='results' />
+			</form>
+		</div>
+	);
 };
 
 export default App;
