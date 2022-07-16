696def463a50249317b10f4c9df49014f79d6ba9
syedkhalidashraf <i.khalidashraf@gmail.com>
2022-07-16 00:12:25 -0400

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import API from "./api";
 
 const App = () => {
+	const test = "TEST";
+	const [list, setList] = useState([]);
+	const [display, setDisplay] = useState([]);
+
+	useEffect(() => {
+		const apiHandler = async () => {
+			const riderTrips = await API.fetchRecentTripsForRider(test);
+			riderTrips.map(async (obj) => {
+				const eMail = await (await API.fetchDriver(obj.driverUUID)).email;
+				setList((prev) => [...prev, eMail]);
+			});
+		};
+		apiHandler();
+	}, []);
+
+	const onClickHandler = (e) => {
+		e.preventDefault();
+		setDisplay(list);
+	};
+
 	return (
 		<div className='App'>
-			<form
-				id='form'
-				onSubmit={() => {
-					console.log("hello");
-				}}>
+			<form id='form' onSubmit={onClickHandler}>
 				<label>
 					Rider UUID:
-					<input type='text' id='text' value='TEST' />
+					<input type='text' id='text' value={test} />
 				</label>
 				<input type='submit' value='Fetch driver emails' />
 				<div id='results' />
 			</form>
+			{display.map((email) => (
+				<h1>{email}</h1>
+			))}
 		</div>
 	);
 };
